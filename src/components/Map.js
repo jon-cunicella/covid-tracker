56f5a014fd117336fa4613b6d76996f92ed019e6
Your Name <joncunicella@gmail.com>
2022-03-14 18:30:35 -0400

    Refactor country marker component to use position as prop

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,18 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import markerIconPng from 'leaflet/dist/images/marker-icon.png';
-import { Icon } from 'leaflet';
+import CountryMarker from './CountryMarker';
 const Map = () => {
   return (
     <MapContainer
-      center={[39.9612, -82.9988]}
-      zoom={5}
+      center={[17.5707, 3.9962]}
+      zoom={3}
       style={{ height: '100vh', width: '100wh' }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker
-        position={[39.9612, -82.9988]}
-        icon={
-          new Icon({
-            iconUrl: markerIconPng,
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-          })
-        }
-      >
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <CountryMarker position={[17.5707, 3.9962]} />
     </MapContainer>
   );
 };
