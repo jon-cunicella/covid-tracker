44e3f329813fb376a78c93f71b5b8fa197b49433
Your Name <joncunicella@gmail.com>
2022-03-15 15:04:33 -0400

    Markers rendering on map for all countries

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,40 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import CountryMarker from './CountryMarker';
+import { useEffect, useState } from 'react';
+
 const Map = () => {
+  const axios = require('axios').default;
+  const [latLongList, setLatLongList] = useState([
+    { longitude: '', latitude: '' },
+  ]);
+  const latLongLists = [];
+
+  const testList = [
+    { latitude: 0, longitude: 0 },
+    { latitude: 10, longitude: 10 },
+  ];
+  async function fetchCountryPins() {
+    try {
+      const response = await axios.get(
+        'https://corona.lmao.ninja/v2/countries?yesterday=&sort='
+      );
+      response.data.map((country) => {
+        latLongLists.push({
+          latitude: country.countryInfo.lat,
+          longitude: country.countryInfo.long,
+        });
+      });
+      setLatLongList(latLongLists);
+      console.log(latLongLists);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCountryPins();
+  }, []);
   return (
     <MapContainer
       center={[17.5707, 3.9962]}
@@ -12,7 +45,9 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <CountryMarker position={[17.5707, 3.9962]} />
+      {latLongList.map((entry) => (
+        <CountryMarker position={[entry.latitude, entry.longitude]} />
+      ))}
     </MapContainer>
   );
 };
